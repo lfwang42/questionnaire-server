1b605681062fa27782347f2f0420ce312135889b
Lanney <lfwang42@gmail.com>
2026-02-26 21:05:25 -0500

    test

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 import os
 import json
 from time import sleep
-path = '/mnt/g/Downloads/Combinations4/Combinations4'
+path = '/mnt/g/Downloads/Combinations5/Combinations5'
 paintings_path = '/mnt/g/Downloads/paintings1/paintings'
 import shutil
 dict = {}
